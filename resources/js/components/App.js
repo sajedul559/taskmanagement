030763810c73a96bc945cc965228a73954cdea78
sajedul <sajedulkhairul@gmail.com>
2021-06-14 20:36:02 +0600

    file setup and run first component

---
+++ resources/js/components/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function App() {
+    return (
+        <div className="container">
+            <div className="row justify-content-center">
+                <div className="col-md-8">
+                    <div className="card">
+                        <div className="card-header">Hello I am from component  </div>
+
+                        <div className="card-body"> Im an example component!</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default App;
+
+if (document.getElementById('app')) {
+    ReactDOM.render(<App />, document.getElementById('app'));
+}
